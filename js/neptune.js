82510786a68bfca5cd556c22c8a056b5073ee156
FEIFAN_monokuma <368381988@qq.com>
2015-07-14 14:26:53 +0800

    增加交互，可加入订单

--- js/neptune.js
+++ js/neptune.js
@@ -1,3 +1,16 @@
 /**
  * Created by Administrator on 15-7-13.
  */
+$(document).ready(function(){
+    $(".menu_items").on("click",function(){
+
+        $(this).css("background-color","#FFCC99").find("[name='menu']").attr("checked","true");
+        $(this).find(".glyphicon-ok").removeClass("hide");
+        $(this).siblings(".menu_items").css("background-color","#FFFFFF").find(".glyphicon-ok").addClass("hide");
+
+    }).on("mouseover",function() {
+            $(this).css({"cursor":"hand"});
+    }).on("mouseout",function(){
+            $(this).css({"cursor":"pointer"});
+    });
+});

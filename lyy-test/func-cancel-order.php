157d0f4ac4baa748c9a19a011b32e0a16ea39820
yyli12 <yyli12@fudan.edu.cn>
2015-07-14 11:13:03 +0800

    Merge branch 'master' of https://github.com/seraphimrose/Neptune
    Conflicts:
    	index.php

--- lyy-test/func-cancel-order.php
+++ lyy-test/func-cancel-order.php
@@ -5,3 +5,16 @@
  * Date: 2015/7/14
  * Time: 10:16
  */
+include("connect-db.php");
+
+function delete_order($order_id, $dbh) {
+    $ret = $dbh->exec("DELETE FROM `neptune`.`order` WHERE `order`.`id` = $order_id");
+    echo $ret;
+    if($ret != 1){
+        $status['status'] = false;
+    } else {
+        $status['status'] = true;
+    }
+    echo json_encode($status);
+}
+

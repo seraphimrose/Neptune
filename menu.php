3b3ff96f53fb7c29773497771ce2339b91e63028
Wudan <13210240067@fudan.edu.cn>
2015-07-15 11:35:45 +0800

    menu实现后台查看以及查看相应评论

--- menu.php
+++ menu.php
@@ -1,4 +1,6 @@
+
 <script src="js/jquery-2.1.4.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
 <script>
     $(document).ready(function () {
         $(".menu_items").on("click", function () {
@@ -12,7 +14,13 @@
         }).on("mouseout", function () {
             $(this).css({"cursor": "pointer"});
         });
-
+        $(".comment").click(function (e) {
+            e.stopPropagation();
+            $(this).parents(".menu_items").next('.collapse').collapse("toggle");
+        });
+        $(".well").click(function (e) {
+            e.stopPropagation();
+        });
     });
 </script>
 <div class="menu_title">
@@ -27,12 +35,11 @@
             try {
                 $dbh = new PDO('mysql:host=localhost;dbname=neptune', "root", "");
                 $dbh->query("set names utf8");
-                foreach ($dbh->query('SELECT * from menu where flag = 0') as $tmp) :
+                foreach ($dbh->query('SELECT * from menu where flag = 0') as $tmp) {
                     ?>
                     <div class="table-bordered menu_items">
                         <div class="row">
                             <div class="col-md-3">
-                                <input type="radio" name="menu" value="option1" class="hide"/>
                                 <?php
                                 if (!empty($tmp[2])) {
                                     ?>
@@ -47,19 +54,62 @@
                             </div>
                             <div class="col-md-7">
                                 <h4><?php echo $tmp[1] ?></h4><br/>
+
                                 <p><?php echo $tmp[4] ?></p>
                             </div>
                             <div class="col-md-1">
-                                <button class="btn btn-primary comment"><span
-                                        class="glyphicon glyphicon-comment"></span></button>
+                                <div class="badge" style="margin-top: 20px;">12人</div>
+                                <button class="btn btn-primary comment" type="button">
+                                    <span class="glyphicon glyphicon-comment"></span>
+                                </button>
                             </div>
                             <div class="col-md-1">
                                 <span class="glyphicon glyphicon-ok hide" style="color:green;margin:10px;"></span>
                             </div>
                         </div>
                     </div>
+                    <div class="collapse">
+                        <div class="well">
+                            <form>
+                                <div class="row">
+                                    <div class="col-md-10">
+                                        <textarea name="comment" rows="auto" cols="80" class="comment_form"></textarea>
+                                    </div>
+                                    <div class="col-md-2">
+                                        <button class="btn btn-primary" type="submit">提交</button>
+                                    </div>
+                                </div>
+                            </form>
+                            <hr/>
+                            <?php
+                            foreach ($dbh->query("SELECT * from comment where dish_id = '$tmp[0]'  ") as $cmt) {
+                                ?>
+                                <div class="row">
+                                    <?php
+                                    foreach ($dbh->query("SELECT * from user where id = '$cmt[1]'  ") as $urt) {
+                                        ?>
+                                        <div class="col-md-2" style="color:red;text-align: center;">
+                                            <?php echo $urt[1] ?><!-- 张飞 :根据id链接后台获取用户名  -->
+                                        </div>
+                                        <?php
+                                    }
+                                    ?>
+                                    <div class="col-md-10">
+                                        <?php echo $cmt[4] ?> <!-- 这菜非常难吃:链接后台获取评论  -->
+                                    </div>
+                                    <p style="color:gray;margin-top:15px;">
+                                        <?php echo $cmt[3] ?> <!-- 后台获取评论时间 -->
+                                    </p>
+
+                                </div>
+                                <?php
+                            }
+                            ?>
+                        </div>
+
+                    </div>
                     <?php
-                endforeach;
+                }
                 $dbh = null;
             } catch (PDOException $e) {
                 print "Error!: " . $e->getMessage();

e0563c5d3954a9a0ee07308cfbb3b785f8f45127
yyli12 <yyli12@fudan.edu.cn>
2015-07-15 22:26:58 +0800

    Merge branch 'master' of https://github.com/seraphimrose/Neptune
    Conflicts:
    	lyy-test/comment.php

---
+++ lyy-test/fetchComment.php
@@ -0,0 +1,43 @@
+<head>
+    <script src="js/jquery-2.1.4.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+</head>
+
+<form>
+    <div class="row">
+        <div class="col-md-10">
+            <textarea name="comment" rows="auto" cols="80" class="comment_form"></textarea>
+        </div>
+        <div class="col-md-2">
+            <button class="btn btn-primary" type="submit">提交</button>
+        </div>
+    </div>
+</form>
+<hr/>
+<?php
+include("connect-db.php");
+foreach ($dbh->query("SELECT * from comment where dish_id = '$_POST[dish_id]'") as $cmt) {
+    ?>
+    <div class="row">
+        <?php
+
+        foreach ($dbh->query("SELECT * from user where id = '$cmt[1]'  ") as $urt) {
+            ?>
+            <div class="col-md-2" style="color:red;text-align: center;">
+                <?php echo $urt[1] ?><!-- 张飞 :根据id链接后台获取用户名  -->
+            </div>
+            <?php
+        }
+        ?>
+        <div class="col-md-offset-1 col-md-9">
+            <?php echo $cmt[4] ?> <!-- 这菜非常难吃:链接后台获取评论  -->
+        </div>
+        <p style="color:gray;margin-top:15px;">
+            <?php echo $cmt[3] ?> <!-- 后台获取评论时间 -->
+        </p>
+    </div>
+    <?php
+}
+?>
+
--- lyy-test/func-test.php
+++ lyy-test/func-test.php
@@ -1,11 +1,37 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: yyli
- * Date: 2015/7/15
- * Time: 14:19
- */
+<link href="//cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet"/>
 
-?>
+<script src="../js/jquery-2.1.4.min.js"></script>
+
+<script type="application/javascript">
+    $(document).ready(function(){
+        $("button.test").click(function(){
+
+            id = $("[name='id']").val();
+
+            $.post("fetchComment.php",
+                {
+                    dish_id: id,
+                },
+                function (data, status) {
+                    $( "div.test" ).html(data);
+                });
+
+        });
+
+    });
 
 
+</script>
+<div>
+
+    <div class="test"></div>
+    <div>
+        <label>id</label>
+        <input class="test" name="id" type="text"/>
+    </div>
+
+    <div>
+        <button type="submit" class="test">submit</button>
+    </div>
+
+</div>

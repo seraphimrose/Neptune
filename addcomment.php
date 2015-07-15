2c6204782b3eef54b659fd801e1151a5615c73ee
Wudan <13210240067@fudan.edu.cn>
2015-07-15 16:29:10 +0800

    评论添加

--- addcomment.php
+++ addcomment.php
@@ -10,20 +10,19 @@ header("Content-Type: text/html;charset=utf-8");
 date_default_timezone_set('prc');
 include "conn.php";
 include_once "lyy-test/func-login-check.php";
-if (!isset($_SESSION['user_id'])) {
-    echo "未登录";
-}
-$userid = "2";
-$dishid = "1";
-$content = "我就是说想试试。";
-$time = time();
-$mysqltime = date('Y-m-d H:i:s', $time);
-$status = null;
-$insrt = "INSERT INTO comment(id, user_id, dish_id, time,content) VALUES (null,'$userid','$dishid','$mysqltime','$content')";
-$res = $dbh->query($insrt);
-if ($dbh === false) {
-    $status["status"] = false;
-} else {
-    $status["status"] = true;
+if ($login) {
+    $userid = $_SESSION['user_id'];
+    $dishid = "1";////////////
+    $content = "我就是说想试试。";///////////
+    $time = time();
+    $mysqltime = date('Y-m-d H:i:s', $time);
+    $status = null;
+    $insrt = "INSERT INTO comment(id, user_id, dish_id, time,content) VALUES (null,'$userid','$dishid','$mysqltime','$content')";
+    $res = $dbh->query($insrt);
+    if ($dbh === false) {
+        $status["status"] = false;
+    } else {
+        $status["status"] = true;
+    }
 }
 

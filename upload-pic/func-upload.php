c38e70f241ac4cc0c769d035effe37050b5fa1b5
yyli12 <yyli12@fudan.edu.cn>
2015-07-16 20:45:50 +0800

    上传图片功能完成！！！！！！！！！！！ ver 2.9999999

--- upload-pic/func-upload.php
+++ upload-pic/func-upload.php
@@ -1,66 +1,35 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: yyli
- * Date: 2015/7/16
- * Time: 16:09
+ * This is just an example of how a file could be processed from the
+ * upload script. It should be tailored to your own requirements.
  */
-$uptypes=array(
-    'image/jpg',
-    'image/jpeg',
-    'image/png',
-    'image/pjpeg',
-    'image/gif',
-    'image/bmp',
-    'image/x-png'
-);
 
-$max_file_size=2000000;     //上传文件大小限制, 单位BYTE
-$destination_folder="uploadimg/"; //上传文件路径
-$watermark=0;      //是否附加水印(1为加水印,其他为不加水印);
-$watertype=1;      //水印类型(1为文字,2为图片)
-$waterposition=1;     //水印位置(1为左下角,2为右下角,3为左上角,4为右上角,5为居中);
-$waterstring="http://www.xplore.cn/";  //水印字符串
-$waterimg="xplore.gif";    //水印图片
-$imgpreview=1;      //是否生成预览图(1为生成,其他为不生成);
-$imgpreviewsize=1/2;    //缩略图比例
+// Only accept files with these extensions
+$whitelist = array('jpg', 'jpeg', 'png', 'gif');
+$name      = null;
+$error     = '上传失败';
+$path      = 'jpg/';
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST')
-{
-    if (!is_uploaded_file($_FILES["upfile"]["tmp_name"])) {
-        echo "-1";
-        exit;
-    }
-    $file = $_FILES["upfile"];
-    if($max_file_size < $file["size"]) {
-        echo "-2";
-        exit;
-    }
-    if(!in_array($file["type"], $uptypes)) {
-        echo "-3";
-        exit;
-    }
+if (isset($_FILES)) {
+    if (isset($_FILES['file'])) {
+        $tmp_name = $_FILES['file']['tmp_name'];
+        $name = basename($_FILES['file']['name']);
+        $error = $_FILES['file']['error'];
+        $extension = pathinfo($name, PATHINFO_EXTENSION);
+        $name = $path . time() . '.' . $extension;
 
-    if(!file_exists($destination_folder)) {
-        mkdir($destination_folder);
-    }
-
-    $filename=$file["tmp_name"];
-    $pinfo=pathinfo($file["name"]);
-    $ftype=$pinfo['extension'];
-    $destination = $destination_folder.time().".".$ftype;
-
-    if(!move_uploaded_file ($filename, $destination)) {
-        echo "-4";
-        exit;
-    }
-    else {
-        echo $destination;
+        if ($error === UPLOAD_ERR_OK) {
+            if (!in_array($extension, $whitelist)) {
+                $error = '上传文件格式错误';
+            } else {
+                move_uploaded_file( $tmp_name, $name );
+            }
+        }
     }
 }
-else {
-    echo "-5";
-}
 
-
-?>
+echo json_encode(array(
+    'name'  => $name,
+    'error' => $error,
+));
+die();

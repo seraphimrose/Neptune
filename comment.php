fc7264d3f9f44e3b45173a1c8a3959a369162e53
yyli12 <yyli12@fudan.edu.cn>
2015-07-16 15:55:33 +0800

    吐槽板优化 ver 2.8

--- comment.php
+++ comment.php
@@ -1,5 +1,5 @@
 <head>
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+    <script src="js/jquery-2.1.4.min.js"></script>
     <script type="text/javascript">
 
         $(document).ready(function(){
